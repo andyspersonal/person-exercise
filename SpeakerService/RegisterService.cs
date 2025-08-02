4288ad73e688e3f25f1508ce5cd8abfc14688b04
andy chapman <andy@braecottage.co.uk>
2025-08-02 11:56:08 +0100

    Improve  error handling when  saving the speaker to the database.

--- SpeakerService/RegisterService.cs
+++ SpeakerService/RegisterService.cs
@@ -1,7 +1,4 @@
 ﻿using GK.Talks.SupportingClasses;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace GK.Talks
 {
@@ -21,42 +18,20 @@ namespace GK.Talks
       /// <param name="speaker">The speaker to register</param>
       /// <returns>A response object that indicates success or failure and the generated speaker id if sucessfull.</returns>
       public RegisterResponse Register(Speaker speaker)
-      {
-         
-         int? speakerId = null;
-         var ot = new List<string>() { "Cobol", "Punch Cards", "Commodore", "VBScript" };
-
+      {         
+         // validate the speaker
          var speakerRegistorError = speaker.ValidateForRegistration();
-
          if (speakerRegistorError != RegisterError.None)
          {
             return new RegisterResponse(speakerRegistorError);
          }
 
-         //if we got this far, the speaker is approved
-         //let's go ahead and register him/her now.
-         //First, let's calculate the registration fee. 
-         //More experienced speakers pay a lower fee.
+         //calculate the registration fee.
          speaker.CalculateRegistrationFee();
 
-
-         //Now, save the speaker and sessions to the db.
-         try
-         {
-            speakerId = _repository.SaveSpeaker(speaker);
-         }
-         catch (Exception e)
-         {
-            //in case the db call fails 
-         }
-         
-        
-              
-        
-
-         //if we got this far, the speaker is registered.
-         return new RegisterResponse((int)speakerId);
+         // save to teh database.
+         var speakerId = _repository.SaveSpeaker(speaker);
+         return new RegisterResponse((int)speakerId); 
       }
    }
-   
 }
